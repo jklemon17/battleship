f0f3308bb76c2357d0b7fad32a1c928678f8e464
PolarisTLX <paul.a.rail@gmail.com>
2018-07-18 16:10:21 -0400

    DOM event listeners responsive to placed ships
    Co-authored-by: Kyle Lemon <james.kyle.lemon@gmail.com>

--- src/gameboard.js
+++ src/gameboard.js
@@ -2,11 +2,12 @@ import { Ship } from './shipClass';
 
 class Gameboard {
   constructor() {
+    this.sunkenShips = 0;
     this.board = [];
     for (let i = 0; i < 10; i++) {
       this.board.push(new Array(10).fill(0));
     }
-    this.ships = [
+    this.ships = [      
       new Ship("ship1", {x: 0, y: 0}, {x: 0, y: 4}),
       new Ship("ship2", {x: 1, y: 0}, {x: 1, y: 3}),
       new Ship("ship3", {x: 2, y: 0}, {x: 2, y: 2}),
@@ -15,19 +16,26 @@ class Gameboard {
     ];
   }
   receiveAttack(event) {
-    let boardTarget = this.board[event.target.x][event.target.y];
-    this.ships.forEach(ship => ship.spots.forEach(spot => {
-        if (spot.x == event.target.x && spot.y == event.target.y) {
-            spot.applyHit({x: event.target.x, y: event.target.y});
+    let boardTarget = this.gameboard.board[this.x][this.y];
+    this.gameboard.ships.forEach(ship => ship.spots.forEach(spot => {
+        if (spot.x == this.x && spot.y == this.y) {
+            ship.applyHit({x: this.x, y: this.y});
             boardTarget = 1;
-            event.target.classList += 'red';
+            this.classList.toggle('red');
+            if (ship.sunk) {
+              this.gameboard.sunkenShips++;
+              if (this.gameboard.sunkenShips >= 5) {
+                console.log("Game over!");
+              }
+            }
         }}
       )
     )
     if (boardTarget != 1) {
       boardTarget = 2;
-      event.target.classList += 'white';
+      this.classList.toggle('white');
     }
+    this.removeEventListener('click', this.gameboard.receiveAttack);
   }
 }
 
